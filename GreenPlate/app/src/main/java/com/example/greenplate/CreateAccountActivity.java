1d790c0f65425f5876c50244b5cc487d523b8f71
clandaverde6 <cesarlopez36012@gmail.com>
2024-02-23 13:40:32 -0500

    implemented account creation and login with firebase connection

--- GreenPlate/app/src/main/java/com/example/greenplate/CreateAccountActivity.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/CreateAccountActivity.java
@@ -1,10 +1,23 @@
 package com.example.greenplate;
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.*;
+import com.google.firebase.*;
+import com.google.android.gms.tasks.OnCompleteListener;
+
 
 
 public class CreateAccountActivity extends AppCompatActivity {
@@ -12,6 +25,8 @@ public class CreateAccountActivity extends AppCompatActivity {
     private CreateAccountViewModel viewModel;
     private EditText newUsernameEditText;
     private EditText newPasswordEditText;
+
+    private FirebaseAuth mAuth;
 
     protected void onCreate(Bundle savedInstanceState) {
         // DO NOT MODIFY
@@ -22,6 +37,11 @@ public class CreateAccountActivity extends AppCompatActivity {
         newPasswordEditText = findViewById(R.id.newPasswordEditText);
         Button toLoginButton = findViewById(R.id.btn_login);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
+
 
         // TODO:
         //  when create account is clicked, the account needs to be added to firebase before
@@ -29,13 +49,37 @@ public class CreateAccountActivity extends AppCompatActivity {
         toLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = newUsernameEditText.getText().toString();
+                String password = newPasswordEditText.getText().toString();
                 if(checkUsernameAndPassword(newUsernameEditText, newPasswordEditText)) {
-                    Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
-                    startActivity(intent);
+                    // if userName and password pass check, we must create an account before going to login
+                    Task<AuthResult> authResultTask = mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign up success, update UI with the signed-in user's information
+                                        Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        //updateUI(user);
+                                        // Proceed to login screen
+                                        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        // If sign up fails, display a message to the user.
+                                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(CreateAccountActivity.this, "Authentication failed."+ task.getException().getMessage(),
+                                                Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
+                                }
+                            });
                 } else {
-                    // not sure what to do here
+                    // Handle invalid username or password
+                    // For example, show an error message
+                    Toast.makeText(CreateAccountActivity.this, "Invalid username or password.", Toast.LENGTH_SHORT).show();
                 }
-            }
+                }
         });
     }
     public boolean checkUsernameAndPassword(EditText newUsernameEditText, EditText newPasswordEditText) {
--- GreenPlate/app/src/main/java/com/example/greenplate/LoginActivity.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/LoginActivity.java
@@ -1,16 +1,29 @@
 package com.example.greenplate;
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel viewModel;
     private EditText usernameEditText;
     private EditText passwordEditText;
+    private FirebaseAuth mAuth;
 
     protected void onCreate(Bundle savedInstanceState) {
         // DO NOT MODIFY
@@ -22,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         Button toHomeButton = findViewById(R.id.btn_login);
         Button toCreateAccountButton = findViewById(R.id.btn_sign_up);
         Button toCloseApplication = findViewById(R.id.btn_close_app);
+        mAuth = FirebaseAuth.getInstance();
 
 
         // TODO:
@@ -31,11 +45,32 @@ public class LoginActivity extends AppCompatActivity {
         toHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkUsernameAndPassword(usernameEditText, passwordEditText)) {
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
+                String email = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if (!email.isEmpty() && !password.isEmpty() && checkUsernameAndPassword(usernameEditText, passwordEditText)) {
+                    Task<AuthResult> authResultTask = mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        //updateUI(user);
+                                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Authentication failed." + task.getException().getMessage(),
+                                                Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
+                                }
+                            });
                 } else {
-                    // not sure what to do here
+                    // Show a message indicating that email or password is empty
+                    Toast.makeText(LoginActivity.this, "Please enter email and password.", Toast.LENGTH_SHORT).show();
                 }
 
             }

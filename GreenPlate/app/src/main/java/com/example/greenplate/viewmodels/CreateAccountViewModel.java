5fd6c70fd5e2c2a2a3efa9a7f1ab4c5fde11e198
mehdibencheqroun <mehdibencheqroun04@gmail.com>
2024-02-25 22:46:31 -0500

    unsignificant changes

--- GreenPlate/app/src/main/java/com/example/greenplate/viewmodels/CreateAccountViewModel.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/viewmodels/CreateAccountViewModel.java
@@ -4,15 +4,12 @@ import static android.content.ContentValues.TAG;
 
 import android.util.Log;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-
 import com.example.greenplate.views.CreateAccountActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
-
 import com.google.firebase.auth.FirebaseAuth;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;

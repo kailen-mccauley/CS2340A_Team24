84db34123b8d0fe3830adf3dddb1145780515ee1
clandaverde6 <cesarlopez36012@gmail.com>
2024-03-01 19:29:40 -0500

    implemented database logic, created viewModels for meal and personal activity

--- GreenPlate/app/src/main/java/com/example/greenplate/views/MealActivity.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/views/MealActivity.java
@@ -2,13 +2,26 @@ package com.example.greenplate.views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.greenplate.R;
+import com.example.greenplate.viewmodels.MealActivityViewModel;
+import com.example.greenplate.viewmodels.PersonalActivityViewModel;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MealActivity extends AppCompatActivity {
+    private MealActivityViewModel viewModel;
+
+    private FirebaseAuth mAuth;
+
+    private EditText mealNameEditText;
+    private EditText caloriesEditText;
+    private Button btn_submit_meal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,13 @@ public class MealActivity extends AppCompatActivity {
         ImageButton toIngredientsButton = findViewById(R.id.btn_ingredients);
         ImageButton toShoppingButton = findViewById(R.id.btn_shopping);
         ImageButton toPersonalButton = findViewById(R.id.btn_personal);
+
+        mAuth = FirebaseAuth.getInstance();
+        viewModel = MealActivityViewModel.getInstance();
+
+        mealNameEditText = findViewById(R.id.mealNameEditText);
+        caloriesEditText = findViewById(R.id.caloriesEditText);
+        btn_submit_meal = findViewById(R.id.btn_submit_meal);
 
         toHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +78,20 @@ public class MealActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MealActivity.this, PersonalActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_submit_meal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve values from input fields
+                String mealName = mealNameEditText.getText().toString();
+                String calories = caloriesEditText.getText().toString();
+
+                // Call storeUserInformation function from viewModel
+                viewModel.storeMeal(mealName, calories);
+
+                // Optional: You can also add validation here before calling the function
             }
         });
 
--- GreenPlate/app/src/main/java/com/example/greenplate/views/PersonalActivity.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/views/PersonalActivity.java
@@ -2,13 +2,27 @@ package com.example.greenplate.views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.greenplate.R;
+import com.example.greenplate.viewmodels.LoginViewModel;
+import com.example.greenplate.viewmodels.PersonalActivityViewModel;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PersonalActivity extends AppCompatActivity {
+
+    private PersonalActivityViewModel viewModel;
+
+    private FirebaseAuth mAuth;
+
+    private EditText weightEditText;
+    private EditText heightEditText;
+    private EditText genderEditText;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,14 @@ public class PersonalActivity extends AppCompatActivity {
         ImageButton toIngredientsButton = findViewById(R.id.btn_ingredients);
         ImageButton toShoppingButton = findViewById(R.id.btn_shopping);
         ImageButton toMealButton = findViewById(R.id.btn_meal);
+
+        mAuth = FirebaseAuth.getInstance();
+        viewModel = PersonalActivityViewModel.getInstance();
+
+        weightEditText = findViewById(R.id.weightEditText);
+        heightEditText = findViewById(R.id.heightEditText);
+        genderEditText = findViewById(R.id.genderEditText);
+        submitButton = findViewById(R.id.btn_submit_personal_info);
 
         toHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +83,22 @@ public class PersonalActivity extends AppCompatActivity {
             }
         });
 
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve values from input fields
+                String weight = weightEditText.getText().toString();
+                String height = heightEditText.getText().toString();
+                String gender = genderEditText.getText().toString();
+
+                // Call storeUserInformation function from viewModel
+                viewModel.storePersonalInfo(height, weight, gender);
+
+                // Optional: You can also add validation here before calling the function
+            }
+        });
+
+
     }
+
 }

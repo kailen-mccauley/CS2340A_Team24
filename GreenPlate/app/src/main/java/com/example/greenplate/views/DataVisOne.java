4d3646a3c5ed55aa80e844633847e43666514f6e
program-01 <cass.riqh@gmail.com>
2024-03-10 22:45:16 -0400

    Data Vis One with real data from the user.

--- GreenPlate/app/src/main/java/com/example/greenplate/views/DataVisOne.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/views/DataVisOne.java
@@ -18,12 +18,17 @@ import com.anychart.enums.TooltipPositionMode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.greenplate.R;
+import com.example.greenplate.viewmodels.MealActivityViewModel;
+import com.example.greenplate.viewmodels.PersonalActivityViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataVisOne extends AppCompatActivity {
     private Button toReturnButton;
+    private MealActivityViewModel viewModel;
+    private PersonalActivityViewModel personalViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,8 @@ public class DataVisOne extends AppCompatActivity {
         setContentView(R.layout.activity_chart);
 
         toReturnButton = findViewById(R.id.btn_datavisoneback);
+        viewModel = MealActivityViewModel.getInstance();
+        personalViewModel = PersonalActivityViewModel.getInstance();
 
         AnyChartView anyChartView = findViewById(R.id.any_chart_view);
         anyChartView.setProgressBar(findViewById(R.id.progress_bar));
@@ -39,41 +46,78 @@ public class DataVisOne extends AppCompatActivity {
         Cartesian cartesian = AnyChart.column();
 
         List<DataEntry> data = new ArrayList<>();
+        personalViewModel.getUserInfo(new PersonalActivityViewModel.UserInfoCallback() {
+            @Override
+            public void onUserInfoReceived(PersonalActivityViewModel.User user) {
+                viewModel.getDailyCalorieIntake(new MealActivityViewModel.DailyCalorieIntakeCallback() {
+                    @Override
+                    public void onDailyCalorieIntakeReceived(int totalCalories) {
+                        data.add(new ValueDataEntry("Today", totalCalories));
+                        data.add(new ValueDataEntry("Goal", user.getCalorieGoal()));
 
-        data.add(new ValueDataEntry("Rouge", 1220));
-        data.add(new ValueDataEntry("Foundation", 4000));
-        data.add(new ValueDataEntry("Mascara", 200));
-        data.add(new ValueDataEntry("Lip gloss", 3200));
-        data.add(new ValueDataEntry("Lipstick", 1000));
-        data.add(new ValueDataEntry("Nail polish", 890));
-        data.add(new ValueDataEntry("Eyebrow pencil", 750));
-        data.add(new ValueDataEntry("Eyeliner", 1050));
-        data.add(new ValueDataEntry("Eyeshadows", 500));
+                        Column column = cartesian.column(data);
 
-        Column column = cartesian.column(data);
+                        column.tooltip()
+                                .titleFormat("{%X}")
+                                .position(Position.CENTER_BOTTOM)
+                                .anchor(Anchor.CENTER_BOTTOM)
+                                .offsetX(0d)
+                                .offsetY(5d)
+                                .format("${%Value}{groupsSeparator: }");
 
-        column.tooltip()
-                .titleFormat("{%X}")
-                .position(Position.CENTER_BOTTOM)
-                .anchor(Anchor.CENTER_BOTTOM)
-                .offsetX(0d)
-                .offsetY(5d)
-                .format("${%Value}{groupsSeparator: }");
+                        cartesian.animation(true);
+                        cartesian.title("Today Vs Goal");
 
-        cartesian.animation(true);
-        cartesian.title("Top 10 Cosmetic Products by Revenue");
+                        cartesian.yScale().minimum(0d);
 
-        cartesian.yScale().minimum(0d);
+                        cartesian.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
 
-        cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
+                        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+                        cartesian.interactivity().hoverMode(HoverMode.BY_X);
 
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+                        cartesian.xAxis(0).title("Intake");
+                        cartesian.yAxis(0).title("Calories");
 
-        cartesian.xAxis(0).title("Product");
-        cartesian.yAxis(0).title("Revenue");
+                        anyChartView.setChart(cartesian);
+                    }
+                });
+            }
+        });
 
-        anyChartView.setChart(cartesian);
+//        data.add(new ValueDataEntry("Today", 1220));
+//        data.add(new ValueDataEntry("Goal", 2000));
+//        data.add(new ValueDataEntry("Mascara", 200));
+//        data.add(new ValueDataEntry("Lip gloss", 3200));
+//        data.add(new ValueDataEntry("Lipstick", 1000));
+//        data.add(new ValueDataEntry("Nail polish", 890));
+//        data.add(new ValueDataEntry("Eyebrow pencil", 750));
+//        data.add(new ValueDataEntry("Eyeliner", 1050));
+//        data.add(new ValueDataEntry("Eyeshadows", 500));
+
+//        Column column = cartesian.column(data);
+//
+//        column.tooltip()
+//                .titleFormat("{%X}")
+//                .position(Position.CENTER_BOTTOM)
+//                .anchor(Anchor.CENTER_BOTTOM)
+//                .offsetX(0d)
+//                .offsetY(5d)
+//                .format("${%Value}{groupsSeparator: }");
+//
+//        cartesian.animation(true);
+//        cartesian.title("Today Vs Goal");
+//
+//        cartesian.yScale().minimum(0d);
+//
+//        cartesian.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
+//
+//        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+//        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+//
+//        cartesian.xAxis(0).title("Intake");
+//        cartesian.yAxis(0).title("Calories");
+//
+//        anyChartView.setChart(cartesian);
 
         toReturnButton.setOnClickListener(new View.OnClickListener() {
             @Override

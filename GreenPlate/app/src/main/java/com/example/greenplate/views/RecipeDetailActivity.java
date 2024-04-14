1c6feffab042a9b1dcab15ece7403483c7d9c72f
program-01 <cass.riqh@gmail.com>
2024-04-14 10:42:59 -0400

    Complete changes to UI.

--- GreenPlate/app/src/main/java/com/example/greenplate/views/RecipeDetailActivity.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/views/RecipeDetailActivity.java
@@ -2,7 +2,6 @@ package com.example.greenplate.views;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
--- GreenPlate/app/src/main/java/com/example/greenplate/views/ShoppingActivity.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/views/ShoppingActivity.java
@@ -4,13 +4,13 @@ import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.CheckBox;
 
-import com.example.greenplate.viewmodels.RecipeActivityViewModel;
 import com.example.greenplate.viewmodels.ShoppingListActivityViewModel;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +19,6 @@ import com.example.greenplate.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ShoppingActivity extends AppCompatActivity {
@@ -38,14 +37,37 @@ public class ShoppingActivity extends AppCompatActivity {
 
     private TextView buildTextView(int weight, String text) {
         TextView textView = new TextView(ShoppingActivity.this);
-        LinearLayout.LayoutParams ingredientNameParams
-                = new LinearLayout.LayoutParams(0,
+        LinearLayout.LayoutParams ingredientNameParams = new LinearLayout.LayoutParams(0,
                 LinearLayout.LayoutParams.WRAP_CONTENT, weight);
         textView.setLayoutParams(ingredientNameParams);
         textView.setText(text);
         textView.setTextSize(22);
         textView.setTextColor(getResources().getColor(R.color.magenta));
         return textView;
+    }
+
+    private void populateShoppingList(Map<String, Integer> items, LinearLayout scrollable) {
+        scrollable.removeAllViews();
+
+        for (Map.Entry<String, Integer> entry : items.entrySet()) {
+            String ingredient = entry.getKey();
+            int quancheck = entry.getValue();
+            String quantity = String.valueOf(quancheck);
+            LinearLayout ingredientLayout = new LinearLayout(ShoppingActivity.this);
+            ingredientLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            ingredientLayout.setOrientation(LinearLayout.HORIZONTAL);
+            TextView ingredientNameTextView = buildTextView(3, ingredient);
+            TextView quantityTextView = buildTextView(2, quantity);
+            CheckBox checkBox = new CheckBox(ShoppingActivity.this);
+            checkBox.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
+            checkBox.setButtonTintList(ColorStateList.valueOf(getResources().getColor(R.color.magenta)));
+
+            ingredientLayout.addView(ingredientNameTextView);
+            ingredientLayout.addView(quantityTextView);
+            ingredientLayout.addView(checkBox);
+
+            scrollable.addView(ingredientLayout);
+        }
     }
 
     @Override
@@ -57,6 +79,7 @@ public class ShoppingActivity extends AppCompatActivity {
         ImageButton toIngredientButton = findViewById(R.id.btn_ingredients);
         ImageButton toMealButton = findViewById(R.id.btn_meal);
         ImageButton toPersonalButton = findViewById(R.id.btn_personal);
+        Button buyItems = findViewById(R.id.btn_buy_items);
 
         LinearLayout scrollable = findViewById(R.id.scrollLay);
 
@@ -76,29 +99,35 @@ public class ShoppingActivity extends AppCompatActivity {
         shoppingListActivityViewModel.fetchShoppingListItems(new ShoppingListActivityViewModel.ShoppingItemsListener() {
             @Override
             public void onShoppingItemsReceived(Map<String, Integer> existingItems) {
-                Map<String, Integer> allIngredients = new HashMap<>(existingItems);
-                Log.d("Shopping", "All Ingredients: "+allIngredients);
-                for (Map.Entry<String, Integer> entry : allIngredients.entrySet()) {
-                    String ingredient = entry.getKey();
-                    int quancheck = entry.getValue();
-                    Log.d("ShoppingActivity", "Ingredient: " + ingredient + ", Quantity: " + quancheck);
-                    String quantity = String.valueOf(quancheck);
-                    LinearLayout ingredientLayout = new LinearLayout(ShoppingActivity.this);
-                    ingredientLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                    ingredientLayout.setOrientation(LinearLayout.HORIZONTAL);
-                    TextView ingredientNameTextView = buildTextView(3, ingredient);
-                    TextView quantityTextView = buildTextView(2, quantity);
-                    CheckBox checkBox = new CheckBox(ShoppingActivity.this);
-                    checkBox.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                    checkBox.setButtonTintList(ColorStateList.valueOf(getResources().getColor(R.color.magenta)));
+                populateShoppingList(existingItems, scrollable);
+            }
+        });
 
-                    ingredientLayout.addView(ingredientNameTextView);
-                    ingredientLayout.addView(quantityTextView);
-                    ingredientLayout.addView(checkBox);
-
-                    scrollable.addView(ingredientLayout);
-                    Log.d("ShoppingActivity", "Ingredient: " + ingredient + ", Quantity: " + quantity);
+        buyItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> toRemove = new ArrayList<>();
+                int childCount = scrollable.getChildCount();
+                for (int i = 0; i < childCount; i++) {
+                    View view = scrollable.getChildAt(i);
+                    if (view instanceof LinearLayout) {
+                        LinearLayout ingredientLayout = (LinearLayout) view;
+                        CheckBox checkBox = (CheckBox) ingredientLayout.getChildAt(2);
+                        if (checkBox.isChecked()) {
+                            String ingredientName = ((TextView) ingredientLayout.getChildAt(0)).getText().toString();
+                            toRemove.add(ingredientName);
+                            shoppingListActivityViewModel.removeShoppingListItem(ingredientName);
+                        }
+                    }
                 }
+                shoppingListActivityViewModel.fetchShoppingListItems(new ShoppingListActivityViewModel.ShoppingItemsListener() {
+                    @Override
+                    public void onShoppingItemsReceived(Map<String, Integer> existingItems) {
+                        populateShoppingList(existingItems, scrollable);
+                        Intent intent = new Intent(ShoppingActivity.this, ShoppingActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
     }

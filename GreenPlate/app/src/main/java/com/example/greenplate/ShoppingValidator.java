a204d55a2246f00b0e344b1dd58ef57820057c05
Dhruv Kapu <65883953+dhruvmk@users.noreply.github.com>
2024-04-16 16:33:26 -0400

    Update ShoppingValidator.java

--- GreenPlate/app/src/main/java/com/example/greenplate/ShoppingValidator.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ShoppingValidator.java
@@ -1,26 +1,37 @@
 package com.example.greenplate;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ShoppingValidator {
-    public boolean isValidUser(FirebaseAuth currentUser) {
+    public static boolean isValidUser(FirebaseUser currentUser) {
         return currentUser != null;
     }
 
-    public boolean isValidQuantity(int quantity) {
+    public static boolean isValidQuantity(int quantity) {
         return quantity > 0;
     }
 
-    public boolean isValidIngredient(String ingredient) {
-        return ingredient != null && !ingredient.equals("");
+    public static boolean isValidIngredient(String ingredient) {
+        boolean result = ingredient != null && !ingredient.equals("") && ingredient.equals(ingredient.trim());
+        if (!result) {
+            return false;
+        }
+        for (int i = 0; i < ingredient.length(); i++) {
+            if (ingredient.charAt(i) >= 65 && ingredient.charAt(i) <= 90) {
+                return true;
+            } else if (ingredient.charAt(i) >= 97 && ingredient.charAt(i) <= 122) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public boolean isValidBuyItemsList(List<String> items) {
+    public static boolean isValidBuyItemsList(List<String> items) {
         Set<String> set = new HashSet<>(items);
-        return set.size() == items.size();
+        return set.size() == items.size() && items.size() != 0;
     }
 }

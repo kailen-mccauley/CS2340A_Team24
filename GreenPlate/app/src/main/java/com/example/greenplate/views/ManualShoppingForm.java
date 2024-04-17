1d8e6d69a44e9a7f579f94b5318c0e06a8ee690f
program-01 <cass.riqh@gmail.com>
2024-04-17 00:21:17 -0400

    Manual ingredient addition into the Shopping Screen.

---
+++ GreenPlate/app/src/main/java/com/example/greenplate/views/ManualShoppingForm.java
@@ -0,0 +1,90 @@
+package com.example.greenplate.views;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.greenplate.InputValidator;
+import com.example.greenplate.R;
+import com.example.greenplate.viewmodels.IngredientsActivityViewModel;
+import com.example.greenplate.viewmodels.ShoppingListActivityViewModel;
+
+public class ManualShoppingForm extends AppCompatActivity {
+
+    private EditText ingredientTitle;
+    private EditText quantityNum;
+    private ShoppingListActivityViewModel shoppingListActivityViewModel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // DO NOT MODIFY
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_form);
+        Button toShoppingScreen = findViewById(R.id.btn_return_shopping_screen);
+
+        Button btnSubmit = findViewById(R.id.btn_submit_manual);
+
+        RelativeLayout parentLayout = findViewById(R.id.activity_manual_shopping);
+
+        ingredientTitle = findViewById(R.id.nameSpace);
+        quantityNum = findViewById(R.id.quantityNumber);
+
+        shoppingListActivityViewModel = ShoppingListActivityViewModel.getInstance();
+
+
+        toShoppingScreen.setOnClickListener(v ->  {
+            Intent intent = new Intent(ManualShoppingForm.this,
+                    ShoppingActivity.class);
+            startActivity(intent);
+        });
+
+        btnSubmit.setOnClickListener(v ->  {
+            String ingredientName = ingredientTitle.getText().toString();
+
+            String quantity = quantityNum.getText().toString();
+            if (!InputValidator.isValidInputWithSpacesBetween(ingredientName)) {
+                Toast.makeText(ManualShoppingForm.this,
+                        "Please enter a valid ingredient name!",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!InputValidator.isValidInputWithInteger(quantity)) {
+                Toast.makeText(ManualShoppingForm.this,
+                        "Please enter a valid integer value for quantity!",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            int inputQuantity = Integer.parseInt(quantity);
+
+            shoppingListActivityViewModel.storeShoppingListItem(ingredientName, inputQuantity,
+                    () -> { });
+            ingredientTitle.setText("");
+            quantityNum.setText("");
+            hideKeyboard();
+        });
+
+        parentLayout.setOnTouchListener((v, event) -> {
+            // Hide keyboard
+            hideKeyboard();
+            return false;
+        });
+    }
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+}
+

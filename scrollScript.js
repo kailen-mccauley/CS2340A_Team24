be3338e57e569e7569f41352b4def357017e0817
Kailen Todd McCauley <kailen.mccauley@gmail.com>
2024-04-19 16:55:40 -0400

    Cleans up diagram section.

--- scrollScript.js
+++ scrollScript.js
@@ -1,8 +1,18 @@
+// JavaScript for image scrolling
 let currentImageIndex = 0;
-const images = document.querySelectorAll('.image-container');
+const container = document.querySelector('.diagram-image-container');
+const images = container.querySelectorAll('.diagram-image');
+
+// Display the first image and mark it as active initially
+images[currentImageIndex].classList.add('active');
 
 function scrollImage(direction) {
+    // Remove 'active' class from the current active image
     images[currentImageIndex].classList.remove('active');
+
+    // Update the currentImageIndex
     currentImageIndex = (currentImageIndex + direction + images.length) % images.length;
+
+    // Add 'active' class to the new active image
     images[currentImageIndex].classList.add('active');
 }

4a77b5a81e5ad382b9624d10717e3648bf4776cc
program-01 <cass.riqh@gmail.com>
2024-04-29 01:04:37 -0400

    Basic changes to website files and addition to an optional feature

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var colorButton = document.getElementById("colorButton");
+    var mainSections = document.querySelectorAll("main > section");
+    var textColor = '#000000';
+    var backgroundColor = '#f0ead2';
+    var white = '#ffffff';
+    var black = '#000000';
+    var standardBackgrounColor = '#f0ead2';
+    var darkModeBackgroundColor = '#262423';
+  
+    colorButton.addEventListener("click", function() {
+      changeBackgroundColor();
+    });
+  
+    function changeBackgroundColor() {
+      if (textColor == black) {
+        textColor = white;
+        backgroundColor = darkModeBackgroundColor;
+        colorButton.textContent = 'Light Mode';
+      } else {
+        textColor = black;
+        backgroundColor = standardBackgrounColor;
+        colorButton.textContent = 'Dark Mode';
+      }
+      mainSections.forEach(function(section) {
+        section.style.color = textColor;
+      });
+      document.body.style.backgroundColor = backgroundColor;
+    }
+  });
+  /* colorButton.addEventListener("click", function() {
+    console.log("Button clicked"); // Check if button click event is captured
+    changeBackgroundColor();
+  }); */

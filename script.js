89bdd9dba72b868e81a6206ad4777315ec4bd61d
Kailen Todd McCauley <kailen.mccauley@gmail.com>
2024-05-04 12:47:25 -0400

    Changes light/dark mode to be based on what the user's internet is set to.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var colorButton = document.getElementById("colorButton");
     var mainText = document.querySelectorAll("main section p, main section h4, main section details p");
     var textColor = '#000000';
     var backgroundColor = '#f0ead2';
@@ -7,28 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
     var black = '#000000';
     var standardBackgrounColor = '#f0ead2';
     var darkModeBackgroundColor = '#262423';
-  
-    colorButton.addEventListener("click", function() {
-      changeBackgroundColor();
-    });
-  
+
     function changeBackgroundColor() {
-      if (textColor == black) {
+      var darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      if (darkModeQuery.matches) {
         textColor = white;
         backgroundColor = darkModeBackgroundColor;
-        colorButton.textContent = 'Light Mode';
       } else {
         textColor = black;
         backgroundColor = standardBackgrounColor;
-        colorButton.textContent = 'Dark Mode';
       }
       mainText.forEach(function(mainText) {
-        mainText.style.color = textColor;
+      mainText.style.color = textColor;
       });
       document.body.style.backgroundColor = backgroundColor;
     }
-  });
-  /* colorButton.addEventListener("click", function() {
-    console.log("Button clicked"); // Check if button click event is captured
     changeBackgroundColor();
-  }); */
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeBackgroundColor);
+  });

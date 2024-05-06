c280bd79e366793a9bbb5eb83f961611d83b5d82
program-01 <cass.riqh@gmail.com>
2024-05-06 03:08:57 -0400

    Fixed errors, descriptions/section headers should be aligned with new dark/light mode computer settings.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var colorButton = document.getElementById("colorButton");
-  var mainSections = document.querySelectorAll("main > section");
+  var mainText = document.querySelectorAll("main section p, main section h4, main section details p");
   var summaryElements = document.querySelectorAll("summary");
+  var summaryColor = '#f0f0f0';
   var textColor = '#000000';
   var backgroundColor = '#f0ead2';
-  var summaryColor = '#f0f0f0';
   var white = '#ffffff';
   var black = '#000000';
   var standardBackgrounColor = '#f0ead2';
@@ -12,32 +11,25 @@ document.addEventListener("DOMContentLoaded", function() {
   var lightBoxColor = '#f0f0f0';
   var grayBoxColor = '#A9A9A9';
 
-  colorButton.addEventListener("click", function() {
-    changeBackgroundColor();
-  });
-
   function changeBackgroundColor() {
-    if (textColor == black) {
+    var darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    if (darkModeQuery.matches) {
       textColor = white;
       backgroundColor = darkModeBackgroundColor;
       summaryColor = grayBoxColor;
-      colorButton.textContent = 'Light Mode';
     } else {
       textColor = black;
       backgroundColor = standardBackgrounColor;
       summaryColor = lightBoxColor;
-      colorButton.textContent = 'Dark Mode';
     }
-    mainSections.forEach(function(section) {
-      section.style.color = textColor;
+    mainText.forEach(function(mainText) {
+      mainText.style.color = textColor;
     });
     document.body.style.backgroundColor = backgroundColor;
     summaryElements.forEach(function(summary) {
       summary.style.backgroundColor = summaryColor;
     });
   }
-});
-/* colorButton.addEventListener("click", function() {
-  console.log("Button clicked"); // Check if button click event is captured
   changeBackgroundColor();
-}); */
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeBackgroundColor);
+}); 

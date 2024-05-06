d00e1e69bda056e20d2eccff60259d617f3d77f0
program-01 <cass.riqh@gmail.com>
2024-05-06 02:52:52 -0400

    Descriptions added with dark/light mode adherence.

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var mainText = document.querySelectorAll("main section p, main section h4, main section details p");
-    var textColor = '#000000';
-    var backgroundColor = '#f0ead2';
-    var white = '#ffffff';
-    var black = '#000000';
-    var standardBackgrounColor = '#f0ead2';
-    var darkModeBackgroundColor = '#262423';
+  var colorButton = document.getElementById("colorButton");
+  var mainSections = document.querySelectorAll("main > section");
+  var summaryElements = document.querySelectorAll("summary");
+  var textColor = '#000000';
+  var backgroundColor = '#f0ead2';
+  var summaryColor = '#f0f0f0';
+  var white = '#ffffff';
+  var black = '#000000';
+  var standardBackgrounColor = '#f0ead2';
+  var darkModeBackgroundColor = '#262423';
+  var lightBoxColor = '#f0f0f0';
+  var grayBoxColor = '#A9A9A9';
 
-    function changeBackgroundColor() {
-      var darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-      if (darkModeQuery.matches) {
-        textColor = white;
-        backgroundColor = darkModeBackgroundColor;
-      } else {
-        textColor = black;
-        backgroundColor = standardBackgrounColor;
-      }
-      mainText.forEach(function(mainText) {
-      mainText.style.color = textColor;
-      });
-      document.body.style.backgroundColor = backgroundColor;
-    }
+  colorButton.addEventListener("click", function() {
     changeBackgroundColor();
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeBackgroundColor);
   });
+
+  function changeBackgroundColor() {
+    if (textColor == black) {
+      textColor = white;
+      backgroundColor = darkModeBackgroundColor;
+      summaryColor = grayBoxColor;
+      colorButton.textContent = 'Light Mode';
+    } else {
+      textColor = black;
+      backgroundColor = standardBackgrounColor;
+      summaryColor = lightBoxColor;
+      colorButton.textContent = 'Dark Mode';
+    }
+    mainSections.forEach(function(section) {
+      section.style.color = textColor;
+    });
+    document.body.style.backgroundColor = backgroundColor;
+    summaryElements.forEach(function(summary) {
+      summary.style.backgroundColor = summaryColor;
+    });
+  }
+});
+/* colorButton.addEventListener("click", function() {
+  console.log("Button clicked"); // Check if button click event is captured
+  changeBackgroundColor();
+}); */
